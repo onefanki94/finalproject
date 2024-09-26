66b801cea309264ebd03f3b01339bdb2c1215801
chaewon Kim <sing2727@naver.com>
2024-09-26 15:10:56 +0900

    ㅗ

--- src/main/java/com/ict/finalproject/FinalprojectApplication.java
+++ src/main/java/com/ict/finalproject/FinalprojectApplication.java
@@ -1,12 +1,13 @@
 package com.ict.finalproject;
 
+import org.eclipse.tags.shaded.org.apache.xpath.operations.String;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalprojectApplication {
 
-	public static void main(String[] args) {
+	public static <String> void main(String[] args) {
 		SpringApplication.run(FinalprojectApplication.class, args);
 	}
 
