b02e8ca4928049079dfcc50098236042ea089e9f
chulsoonCHOI <xenic85@gmail.com>
2024-09-27 15:07:04 +0900

    modify

--- src/main/java/com/ict/finalproject/controller/storeMainController.java
+++ src/main/java/com/ict/finalproject/controller/storeMainController.java
@@ -1,7 +1,10 @@
 package com.ict.finalproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/store")
+@Controller
 public class storeMainController {
      @GetMapping("/storeMain")
  public String storeMain() {
