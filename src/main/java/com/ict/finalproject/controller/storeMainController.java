cf68caedc29fdee6c044c27694bb3af12e82b2ee
chaewon Kim <sing2727@naver.com>
2024-10-08 14:42:17 +0900

    ㅇㅇ

--- src/main/java/com/ict/finalproject/controller/storeMainController.java
+++ src/main/java/com/ict/finalproject/controller/storeMainController.java
@@ -1,20 +1,12 @@
 package com.ict.finalproject.controller;
 
-import com.ict.finalproject.Service.StoreService;
-import com.ict.finalproject.vo.StoreVO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 
 @Controller
 public class storeMainController {
-
-    @Autowired
-    StoreService storeService;
      @GetMapping("/storeMain")
  public String storeMain() {
      return "store/storeMain";
@@ -22,19 +14,25 @@ public class storeMainController {
 
 
     @GetMapping("/storeList")
-    public ModelAndView getStoreList(){
-        System.out.println(11);
-        List<StoreVO> storeList = storeService.getStoreList();
-        System.out.println(storeList);
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("storeList", storeList);
-        mav.setViewName("store/storeList");
-        return mav;
-    }
+ public String storeList(){
+    return "store/storeList";
+ }
 
-    @GetMapping("/storeDetail")
+   @GetMapping("/storeDetail")
    public String storDetail() {
        return "store/storeDetail";
    }
-   
+
+    @GetMapping("/shoppingBag")
+    public String shoppingBag() {
+        return "store/shopping_bag";
+    }
+
+    @GetMapping("/orderpage")
+    public String orderpage() {
+        return "store/orderpage";
+    }
 }
+
+
+
