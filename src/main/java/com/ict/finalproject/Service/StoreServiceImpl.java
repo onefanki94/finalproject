77a9e9cf6ca03c2f595c6b96b2cb14a9d0764f65
chulsoonCHOI <xenic85@gmail.com>
2024-10-18 20:22:19 +0900

    Update StoreServiceImpl.java

--- src/main/java/com/ict/finalproject/Service/StoreServiceImpl.java
+++ src/main/java/com/ict/finalproject/Service/StoreServiceImpl.java
@@ -85,7 +85,41 @@ public class StoreServiceImpl implements StoreService {
     public String getCategoryType(int categoryCode) {
         // categoryCode를 통해 카테고리 타입 조회
         return dao.getCategoryType(categoryCode);
+
+
+    @Override
+    public int basketInput(BasketVO basketvo) {
+        return dao.basketInput(basketvo);
     }
+
+    @Override
+    public List<BasketDTO> basketList(int useridx) {
+        return dao.basketList(useridx);
+    }
+
+    @Override
+    public int basketDelete(int idx, int useridx) {
+        return dao.basketDelete(idx, useridx);
+    }
+
+    @Override
+    public void basketChoiceAndAllDelOk(int idx, int useridx) {
+        dao.basketChoiceAndAllDelOk(idx,useridx);
+    }
+
+    @Override
+    public int basketPlusAmount(int idx, int useridx, int newTotal) {
+        return dao.basketPlusAmount(idx, useridx,newTotal);
+    }
+
+    @Override
+    public int basketMinusAmount(int idx, int useridx, int newTotal) {
+        return dao.basketMinusAmount(idx, useridx,newTotal);
+    }
+
+    
+
+
 }
 
 
